34dd22cce1e3f10c3c37a54845a2398875ba9630
Mat V <mateusv@microsoft.com>
2015-06-26 13:12:38 -0700

    First push

---
+++ AzureLens/App_Start/BundleConfig.cs
@@ -0,0 +1,39 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace AzureLens
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.multilevelpushmenu.js"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/cssbundle").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css",
+                      "~/fonts/font-awesome.css",
+                      "~/Content/basecss/jquery.multilevelpushmenu.css",
+                      "~/Content/basecss/main.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/babylon").Include(
+                     "~/Scripts/Babylon.2.1.fix.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                     "~/Scripts/app.js"));
+
+        }
+    }
+}
---
+++ AzureLens/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AzureLens.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "AzureLens.Net prototype";
+
+            return View();
+        }
+    }
+}
